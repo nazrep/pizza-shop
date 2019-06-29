ecff3dfb2079adec9f32b7790567fba7f1d555c3
Kacper Perzankowski <perzankowski.kacper@gmail.com>
2019-06-29 19:55:13 +0200

    setup initial application
    - flask
    - sql alchemy
    - migrations

---
+++ app.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+load_dotenv()
+
+app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+if __name__ == '__main__':
+    app.run()
